6ae6e78c621f5290a8e5c2c9e4e6db60e7404b71
Jaishnav K <157721937+Jaishnav30@users.noreply.github.com>
2024-08-14 13:15:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app = Flask(__name__)
 
